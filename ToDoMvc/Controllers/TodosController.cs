78e415e7167b81ba719deae201b29c351ac80892
lv <valeria.livshyts@gmail.com>
2025-05-18 23:52:53 +0300

    valeria - add TodosController

---
+++ ToDoMvc/Controllers/TodosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoMvc.Data;
+using ToDoMvc.Models;
+
+namespace ToDoMvc.Controllers
+{
+    public class TodosController : Controller
+    {
+        private readonly ToDoContext _context;
+
+        public TodosController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Todos
+        public async Task<IActionResult> Index()
+        {
+            var toDoContext = _context.Todo.Include(t => t.User);
+            return View(await toDoContext.ToListAsync());
+        }
+    }
+}
